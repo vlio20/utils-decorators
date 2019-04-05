6cba6d79bd8475f5fb59d218db2a9dac85105615
Vlad Ioffe <vlio20@gmail.com>
2019-04-05 20:08:02 +0300

    fixing memoizeAsync

--- src/memoize-async/memoize-async.model.ts
+++ src/memoize-async/memoize-async.model.ts
@@ -2,5 +2,5 @@ import {Memoizable, MemoizeConfig} from '..';
 import {Method} from '../common/model/common.model';
 
 export type AsyncMethod<D> = Method<Promise<D>>;
-export type MemoizeAsyncConfig<D> = MemoizeConfig<Promise<D>>;
+export type MemoizeAsyncConfig<D> = MemoizeConfig<D>;
 export type AsyncMemoizable<D> = Memoizable<Promise<D>>;
