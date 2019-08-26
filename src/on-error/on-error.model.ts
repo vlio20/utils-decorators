69ba1a6888f8c9b8c7c20f695839fcac20a711c5
Vlad Ioffe <vlio20@gmail.com>
2019-08-26 16:40:38 +0300

    resolving #37 and #38 (#41)

--- src/on-error/on-error.model.ts
+++ src/on-error/on-error.model.ts
@@ -1,11 +1,12 @@
 import {Method} from '..';
 
 export type OnErrorable<T> = (target: T,
-                         propertyName: keyof T,
-                         descriptor: TypedPropertyDescriptor<Method<any>>) => TypedPropertyDescriptor<Method<any>>;
+                              propertyName: keyof T,
+                              descriptor: TypedPropertyDescriptor<Method<any>>) => TypedPropertyDescriptor<Method<any>>;
 
 export interface OnErrorConfig<T> {
   func: Function | keyof T;
+  wait?: boolean;
 }
 
 export type OnErrorHandler = (e?: Error, args?: any[]) => any;
