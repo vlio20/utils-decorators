74eee61570c18b93527fc4f508b9c186e0a56dd5
Vlad Ioffe <vioffe@outbrain.com>
2019-09-21 23:56:47 +0300

    adding trasholds to stryker

--- src/readonly/readonly.spec.ts
+++ src/readonly/readonly.spec.ts
@@ -1,7 +1,7 @@
 import {readonly} from './readonly';
 
 describe('readonly', () => {
-  xit('should verify readonly throws exception when trying to write to it', () => {
+  it('should verify readonly throws exception when trying to write to it', () => {
     class T {
 
       @readonly()
