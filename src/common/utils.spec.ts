84246efc8fedd9dec3addddd0c06216045cd7e1c
Vlad Ioffe <vioffe@outbrain.com>
2020-03-13 21:13:32 +0200

    upgrading node libs

--- src/common/utils.spec.ts
+++ src/common/utils.spec.ts
@@ -24,6 +24,27 @@ describe('utils', () => {
     done();
   });
 
+  it('should verify task executed in time - A:20, B:40 -> A, B', async (done) => {
+    const runner = new TaskExec();
+    let val = '';
+    const funA = jest.fn().mockImplementation(() => val += 'A');
+    const funB = jest.fn().mockImplementation(() => val += 'B');
+
+    runner.exec(funA, 20);
+    runner.exec(funB, 40);
+    await sleep(10);
+    expect(funA).not.toBeCalled();
+    expect(funB).not.toBeCalled();
+    await sleep(20);
+    expect(funA).toBeCalled();
+    expect(funB).not.toBeCalledTimes(1);
+    await sleep(20);
+    expect(funA).toBeCalledTimes(1);
+    expect(funB).toBeCalledTimes(1);
+    expect(val).toBe('AB');
+    done();
+  });
+
   it('should verify task executed in time -  - A:20, B:20, C:10 -> A, B, C', async (done) => {
     const runner = new TaskExec();
     const funA = jest.fn();
