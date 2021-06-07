2ee40c2b0754013b0a24dc8449ae7f8f0e6ade7c
Vlad Ioffe <vioffe@outbrain.com>
2021-06-07 15:53:32 +0300

    fixing lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,9 @@ module.exports = {
       {ignorePropertyModificationsForRegex: ['^descriptor$']}
     ],
     'max-classes-per-file': ['off'],
-    'class-methods-use-this': ['error', {exceptMethods: ['foo', 'goo']}],
+    'class-methods-use-this': ['error', {exceptMethods: ['foo', 'goo', 'boo']}],
     'no-async-promise-executor': ['off'],
     '@typescript-eslint/no-explicit-any': ['off'],
-    'func-names': ['off']
+    'func-names': ['off'],
   }
 };
